25ddee7b00a8b2c270e13d933dae7c8babb8eec4
Oneiros90 <oneiros.valente@gmail.com>
2019-10-08 16:58:44 +0200

    UI and more math

---
+++ Scripts/Math/CurveUniformResampler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using UnityEngine;
+
+
+namespace RedHoney.Math
+{
+
+    ///////////////////////////////////////////////////////////////////////////
+    public class CurveUniformResampler
+    {
+        private float[] tVals;
+        private float[] cumDist;
+        private int precision;
+
+        public float curveTotalLength => cumDist[precision - 1];
+
+        ///////////////////////////////////////////////////////////////////////////
+        public CurveUniformResampler(Func<float, float> func, int prec, float ratio = 1.0f)
+        {
+            precision = prec;
+
+            tVals = new float[precision];
+            Vector2[] points = new Vector2[precision];
+            for (int i = 0; i < precision; i++)
+            {
+                tVals[i] = i / (float)(precision - 1);
+                points[i] = new Vector2(tVals[i], func(tVals[i]) * ratio);
+            }
+
+            cumDist = new float[precision];
+            for (int i = 1; i < precision; i++)
+            {
+                float dist = (points[i] - points[i - 1]).magnitude;
+                cumDist[i] = cumDist[i - 1] + dist;
+            }
+        }
+
+        ///////////////////////////////////////////////////////////////////////////
+        public float Evaluate(float t)
+        {
+            float targetLen = t * curveTotalLength;
+            int i = Array.BinarySearch(cumDist, targetLen);
+            if (i < 0)
+                i = ~i;
+            if (i > 0)
+                return Mathf.Lerp(tVals[i - 1], tVals[i], (targetLen - cumDist[i - 1]) / (cumDist[i] - cumDist[i - 1]));
+            else
+                return 0.0f;
+        }
+
+
+    }
+}
---
+++ Scripts/UI/DottedLine.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using RedHoney.Math;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace RedHoney.UI
+{
+    ///////////////////////////////////////////////////////////////////////////
+    [RequireComponent(typeof(RectTransform))]
+    public partial class DottedLine : MonoBehaviour
+    {
+        // Inspector fields
+        public Sprite Dot;
+        [Range(0.01f, 1f)]
+        public float Size = 0.01f;
+        [Range(1f, 100f)]
+        public float DotsDistance = 1.0f;
+        public bool DrawOnStart = true;
+        public Color DotsColor = Color.white;
+
+        public AnimationCurve curve;
+        private CurveUniformResampler resampler;
+
+
+        //Utility fields
+        private readonly List<RectTransform> dots = new List<RectTransform>();
+
+        ///////////////////////////////////////////////////////////////////////////
+        private IEnumerator Start()
+        {
+            if (DrawOnStart)
+            {
+                yield return null;
+                Draw();
+            }
+        }
+
+
+        ///////////////////////////////////////////////////////////////////////////
+        // Custom in editor behaviour for redrawing dynamically on changes
+#if UNITY_EDITOR
+        private Vector2 resolution;
+        private void OnValidate()
+        {
+            if (Application.isPlaying && gameObject.scene.rootCount != 0)
+                Draw();
+        }
+        private void Update()
+        {
+            if (resolution.x != Screen.width || resolution.y != Screen.height)
+            {
+                resolution.x = Screen.width;
+                resolution.y = Screen.height;
+                Draw();
+            }
+        }
+#endif
+
+        ///////////////////////////////////////////////////////////////////////////
+        public void Draw()
+        {
+            RectTransform rect = GetComponent<RectTransform>();
+            if (!rect)
+                return;
+
+            foreach (var dot in dots)
+                Destroy(dot.gameObject);
+            dots.Clear();
+
+            float rectRatio = rect.rect.height / rect.rect.width;
+            resampler = new CurveUniformResampler(curve.Evaluate, 500, rectRatio);
+
+            float curveTotalLength = resampler.curveTotalLength * rect.rect.width;
+            int dotsCount = Mathf.FloorToInt(curveTotalLength / DotsDistance);
+            if (dotsCount <= 0)
+                return;
+
+            Vector2[] positions = new Vector2[dotsCount];
+
+            float step = 1.0f / (dotsCount - 1);
+            for (int i = 0; i < positions.Length; i++)
+            {
+                float t = step * i;
+                float t2 = resampler.Evaluate(t);
+                positions[i].Set(t2, curve.Evaluate(t2));
+            }
+
+            RectTransform newDot()
+            {
+                var dot = new GameObject("dot");
+                dot.transform.localScale = Vector2.one * Size;
+                dot.transform.SetParent(transform, false);
+
+                var sr = dot.AddComponent<Image>();
+                sr.sprite = Dot;
+                sr.color = DotsColor;
+
+                var dotRect = dot.GetComponent<RectTransform>();
+                dotRect.anchorMin = Vector2.zero;
+                dotRect.anchorMax = Vector2.zero;
+
+                return dotRect;
+            }
+
+            foreach (var position in positions)
+            {
+                var dot = newDot();
+                dot.anchoredPosition = Vector2.Scale(position, rect.rect.size);
+                dots.Add(dot);
+            }
+        }
+    }
+}
