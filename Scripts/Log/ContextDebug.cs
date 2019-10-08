ce5826d56909a1f9e1e8f35223db591e9b07d530
Oneiros90 <oneiros.valente@gmail.com>
2019-10-08 16:33:17 +0200

    Adding some logging utilities

---
+++ Scripts/Log/ContextDebug.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+
+namespace RedHoney.Log
+{
+    /// <summary>
+    /// This class provides a custom Debug logger object with pretty prefix.
+    /// You can use import it like this:
+    /// using Debug = ContextDebug<MyClass>;
+    /// </summary>
+    /// <typeparam name="T">The name of this type will be used as prefix in the logs</typeparam>
+    public class ContextDebug<T> : Debug
+    {
+        private static readonly string prefix = $"<b>[{typeof(T).Name}]</b>";
+        private static string logTemplate(object message) => $"{prefix} {message}";
+
+        public new static void Log(object message) => Debug.Log(logTemplate(message));
+        public new static void LogWarning(object message) => Debug.LogWarning(logTemplate(message));
+        public new static void LogError(object message) => Debug.LogError(logTemplate(message));
+
+    }
+}
---
+++ Scripts/Log/ScreenLogger.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace RedHoney.Log
+{
+    ///////////////////////////////////////////////////////////////////////////////
+    public class ScreenLogger : MonoBehaviour
+    {
+        public Color textColor = Color.white;
+        public Color logColor = Color.white;
+        public Color warningColor = Color.yellow;
+        public Color errorColor = Color.red;
+
+        public int textSize = 20;
+        public int maxLogsNumber = 20;
+
+        [Range(0, 1)]
+        public float width = 0.5f;
+
+        private Dictionary<LogType, string> logTypeColors;
+        private GUIStyle labelStyle;
+        private readonly Queue logs = new Queue();
+        private string logsString;
+
+        ///////////////////////////////////////////////////////////////////////////
+        void Awake()
+        {
+            labelStyle = new GUIStyle();
+            labelStyle.normal.textColor = textColor;
+            labelStyle.fontSize = textSize;
+            labelStyle.wordWrap = true;
+
+            string colorToCode(Color c) => $"#{ColorUtility.ToHtmlStringRGBA(c)}";
+            logTypeColors = new Dictionary<LogType, string>
+            {
+                [LogType.Log] = colorToCode(logColor),
+                [LogType.Warning] = colorToCode(warningColor),
+                [LogType.Error] = colorToCode(errorColor),
+                [LogType.Exception] = colorToCode(errorColor),
+                [LogType.Assert] = colorToCode(errorColor),
+            };
+        }
+
+        ///////////////////////////////////////////////////////////////////////////
+        void OnEnable()
+        {
+            Application.logMessageReceivedThreaded += HandleLog;
+        }
+
+        ///////////////////////////////////////////////////////////////////////////
+        void OnDisable()
+        {
+            Application.logMessageReceivedThreaded -= HandleLog;
+        }
+
+        ///////////////////////////////////////////////////////////////////////////
+        void HandleLog(string newLog, string stackTrace, LogType type)
+        {
+            StringBuilder newScreenLog = new StringBuilder();
+            newScreenLog.Append($"<color={logTypeColors[type]}>");
+            newScreenLog.Append($"{newLog}\n");
+            if (type == LogType.Exception)
+                newScreenLog.Append($"\t{stackTrace}\n");
+            newScreenLog.Append("</color>");
+
+            logs.Enqueue(newScreenLog);
+            if (logs.Count > maxLogsNumber)
+                logs.Dequeue();
+
+            logsString = string.Join("", logs.ToArray());
+        }
+
+        ///////////////////////////////////////////////////////////////////////////
+        void OnGUI()
+        {
+            GUILayout.Label(logsString, labelStyle, GUILayout.MaxWidth(Screen.width * width));
+        }
+    }
+}
