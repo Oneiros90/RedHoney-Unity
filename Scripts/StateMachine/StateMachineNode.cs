e0ae2b01c642ec8541a3e109e4124fc5b1783aa9
Oneiros90 <oneiros.valente@gmail.com>
2019-10-10 11:51:47 +0200

    Damn Unity Mecanim...

--- Scripts/StateMachine/StateMachineNode.cs
+++ Scripts/StateMachine/StateMachineNode.cs
@@ -1,104 +1,41 @@
 ﻿using UnityEngine;
 using UnityEngine.Events;
 
-namespace RedHoney.StateMachine
+///////////////////////////////////////////////////////////////////////////
+/// This StateMachineBehaviour exposes enter/exit state events as UnityEvents
+[SharedBetweenAnimators]
+public class StateMachineNode : StateMachineBehaviour
 {
-    using Debug = Log.ContextDebug<StateMachineNode>;
+    [Tooltip("The event of entering in the state (or sub-state machine)")]
+    public UnityEvent OnEnter;
+
+    [Tooltip("The event of exiting from the state (or sub-state machine)")]
+    public UnityEvent OnExit;
+
+
+    private StateMachine StateMachine;
 
     ///////////////////////////////////////////////////////////////////////////
-    /// This StateMachineBehaviour exposes enter/exit state events as UnityEvents
-    [SharedBetweenAnimators]
-    public class StateMachineNode : StateMachineBehaviour
+    public void Init(StateMachine stateMachine)
     {
-        [Tooltip("The event of entering in the state (or sub-state machine)")]
-        public UnityEvent OnEnter;
-
-        [Tooltip("The event of exiting from the state (or sub-state machine)")]
-        public UnityEvent OnExit;
-
-
-        private bool inStateMachine;
-        private bool inState;
-        private readonly bool debugLogs = false;
-
-        ///////////////////////////////////////////////////////////////////////////
-        public static void InitializeAll(Animator animator)
-        {
-            var nodes = animator.GetBehaviours<StateMachineNode>();
-            foreach (StateMachineNode node in nodes)
-                node.Start();
-        }
-
-        ///////////////////////////////////////////////////////////////////////////
-        private void Start()
-        {
-            inStateMachine = false;
-            inState = false;
-        }
-
-        ///////////////////////////////////////////////////////////////////////////
-        // Called when a transition starts and the state machine starts to evaluate this state
-        override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-        {
-            if (inStateMachine)
-                return;
-
-            if (inState)
-                return;
-
-            if (debugLogs)
-                Debug.Log($"[{GetHashCode()}, {stateInfo.fullPathHash}] OnStateEnter");
-
-            OnEnter.Invoke();
-            inState = true;
-        }
-
-        ///////////////////////////////////////////////////////////////////////////
-        // Called when a transition ends and the state machine finishes evaluating this state
-        override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-        {
-            if (inStateMachine)
-                return;
-
-            if (!inState)
-                return;
-
-            if (debugLogs)
-                Debug.Log($"[{GetHashCode()}, {stateInfo.fullPathHash}] OnStateExit");
-
-            OnExit.Invoke();
-            inState = false;
-        }
-
-        ///////////////////////////////////////////////////////////////////////////
-        // Called on the first Update frame when making a transition to a state machine.
-        public override void OnStateMachineEnter(Animator animator, int stateMachinePathHash)
-        {
-            if (inStateMachine)
-                return;
-
-            Debug.LogWarning("Warning: You are using a StateMachineNode in a SubStateMachine. You may encounter on unexpected behaviours");
-
-            if (debugLogs)
-                Debug.Log($"[{GetHashCode()}, {stateMachinePathHash}] OnStateMachineEnter");
-
-            OnEnter.Invoke();
-            inStateMachine = true;
-        }
-
-        ///////////////////////////////////////////////////////////////////////////
-        // Called on the first Update frame when making a transition from a state machine.
-        public override void OnStateMachineExit(Animator animator, int stateMachinePathHash)
-        {
-            if (!inStateMachine)
-                return;
-
-            if (debugLogs)
-                Debug.Log($"[{GetHashCode()}, {stateMachinePathHash}] OnStateMachineExit");
-
-            OnExit.Invoke();
-            inStateMachine = false;
-        }
-
+        StateMachine = stateMachine;
     }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        //name = nameof(StateMachineNode) + stateInfo.fullPathHash.ToString();
+        if (animator == StateMachine.Animator)
+            StateMachine.TryToEnter(this);
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (animator == StateMachine.Animator)
+            StateMachine.TryToExit(this);
+    }
+
 }
